aa7d4deb02390cba87b90c7140498e22f28e6788
AdminGod <github@admingod.simplelogin.com>
2024-11-14 11:06:53 +0100

    Added react router and deleted the App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import './attractions.jsx'
+import './details.jsx'
+import './map.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/attractions" element={<Attractions />} />
+        <Route path="/attractions/:id" element={<Details />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </Router>
   );
 }
 
